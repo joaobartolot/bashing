3901e4bc31b94c03777b236013ade4a8cd098555
Joao Bartolot (linux) <jvcbartolot@gmail.com>
2019-08-23 18:35:52 -0300

    Some changes

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,8 @@ alias nconfig='nvim ~/.config/nvim/init.vim'
 alias rebash='source ~/.bashrc'
 alias funcConfig='nvim ~/.bash_custom/func.sh'
 alias aliasConfig='nvim ~/.bash_custom/aliases.sh'
+alias q='exit'
+alias c='clear'
+alias ..='cd ..'
+
+alias proj='cd ~/Projects/'
